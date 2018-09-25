39f7a9505cac1a8c1f0ac2cda2c9345b46f5af02
biel97 <bieldossantos-97@hotmail.com.br>
2018-09-25 11:15:31 -0300

    Adicionando Alguns Managed Bean que Serao Implementados

---
+++ ImplicareView/src/java/br/cefetmg/implicare/ManagedBean/CargoAreaEstudoMB.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.implicare.ManagedBean;
+
+/**
+ *
+ * @author Aluno
+ */
+public class CargoAreaEstudoMB {
+
+    public Set<CargoAreaEstudo> CargoAreaEstudo(List<FormacaoAcademica> FormAcad) throws BusinessException, PersistenceException;
+    
+}
